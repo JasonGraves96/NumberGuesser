27428e95f46dc4b73d9c6f3257aa517e1f060391
Jason Graves <92340845+JasonGraves96@users.noreply.github.com>
2021-10-23 11:40:16 -0400

    Add files via upload

---
+++ Guessing the Number/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guessing_the_Number
+{
+    public partial class Form1 : Form
+    {
+        int number;
+        int high = 100;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getNewNumber();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int guess = 0;
+            try
+            {
+                guess = Int32.Parse(textBox1.Text);
+            }catch(FormatException idk)
+            {}
+            int counter = Int32.Parse(label3.Text);
+            counter++;
+            if (guess == number)
+            {
+                label1.Text = "YES!!!";
+                if(high > counter)
+                    high = counter;
+                counter = 0;
+                getNewNumber();
+            }else if(guess > number)
+            {
+                label1.Text = "WRONG";
+                label2.Text = "lower";
+            }
+            else
+            {
+                label1.Text = "WRONG";
+                label2.Text = "higher";
+            }
+            if(high != 100)
+            label4.Text = high.ToString();
+            label3.Text = counter.ToString();
+        }
+        private void getNewNumber()
+        {
+            Random rnd = new Random();
+            number = rnd.Next(1000);
+        }
+        
+    }
+}
